6b4269ba04400b3fe11e58475c21a0fec7675e31
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-11-27 18:43:34 -0300

    publicou

--- src/main/java/br/edu/ifpb/pdist/hospitalgateway/GatewayConfig.java
+++ src/main/java/br/edu/ifpb/pdist/hospitalgateway/GatewayConfig.java
@@ -12,7 +12,7 @@ public class GatewayConfig {
         return builder.routes()
                 .route("user", r -> r.path("/user/**")
                         // .uri("http://localhost:8085"))//local
-                        .uri("https://hospitalbackuser-d316271b873b.herokuapp.com"))
+                        .uri("https://userback-6fb5d32424ff.herokuapp.com"))
                 .route("medico", r -> r.path("/medico/**")
                         // .uri("http://localhost:8085"))
                         .uri("https://apibackmedico-017465360da7.herokuapp.com"))
